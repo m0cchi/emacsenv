b088887ed7ddd55970313f9362f88ff89766ad65
m0cchi <boom.boom.planet@gmail.com>
2016-10-12 22:44:05 +0900

    env's result can put a prefix

--- bin/emacsenv.d/env.sh
+++ bin/emacsenv.d/env.sh
@@ -8,11 +8,13 @@ EMACS_HOME=""
 . $ENV_FILE
 
 function show_env() { 
-    echo $1=$(eval 'echo $'"$1")
+    echo "$1$2"='"'$(eval 'echo $'"$2")'"'
 }
+prefix=$1
+shift 1
 EMACS_VERSION=`basename $EMACS_HOME`
 HOME=$EMACS_HOME/profiles
-show_env 'EMACSENV_HOME'
-show_env 'EMACS_VERSION'
-show_env 'EMACS_HOME'
-show_env 'HOME'
+show_env "$prefix" 'EMACSENV_HOME'
+show_env "$prefix" 'EMACS_VERSION'
+show_env "$prefix" 'EMACS_HOME'
+show_env "$prefix" 'HOME'
