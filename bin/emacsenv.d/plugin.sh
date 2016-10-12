a7cbbbc604fc8d57eb7f9aae375497e22d43eda8
m0cchi <boom.boom.planet@gmail.com>
2016-10-13 00:13:01 +0900

    support user's plugin invoker

--- bin/emacsenv.d/plugin.sh
+++ bin/emacsenv.d/plugin.sh
@@ -2,6 +2,12 @@ if [ "$COMMAND" = "" ]; then
     exit 1
 fi
 
+# user's plugin.sh
+if [ -e "$EMACSENV_PLUGINS_DIR/plugin.sh" ]; then
+    . "$EMACSENV_PLUGINS_DIR/plugin.sh"
+    return $?
+fi
+
 if [ -e "$EMACSENV_PLUGINS_DIR/$COMMAND.sh" ]; then
     . "$EMACSENV_PLUGINS_DIR/$COMMAND.sh"
 else
